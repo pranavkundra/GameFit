818fd2cfff3c635a7f3ce5c3c6f5c8e60dd97518
chinmayrane <chinmayrane17@gmail.com>
2015-09-26 02:10:38 -0400

    Scratch NodeJS Server Structure

---
+++ server.js
@@ -0,0 +1,93 @@
+/**
+Module Dependencies
+*/
+
+var express = require('express');
+
+/*MongoDB Node.JS Connector*/
+var mongoose = require('mongoose');
+
+//NodeJS Error Handler
+var errorhandler = require('errorhandler');
+
+/*PassportJS*/
+var passport = require('passport');
+
+/* Production Settings */
+// /var config = require('config');
+
+/**API keys and Passport configuration.*/
+var secrets = require('./config/secrets');
+
+/* SSL Required files*/
+var fs = require('fs');
+var https = require('https');
+// var privateKey  = fs.readFileSync('sslcert/private-key.pem', 'utf8');
+// var certificate = fs.readFileSync('sslcert/certificate.pem', 'utf8');
+// var credentials = {key: privateKey, cert: certificate};
+
+
+/** Create Express server.*/
+var app = module.exports = express();
+
+/*** Express configuration.*/
+app.set('port', process.env.PORT || 3000);
+app.set('httpsport', process.env.PORT || 4433);
+
+// Bootstrap passport config
+require('./config/passport')(passport);
+
+// Bootstrap application settings
+require('./config/express')(app, passport);
+
+// Bootstrap routes
+require('./config/routes')(app, passport);
+
+// app.use(function(req, res, next) {
+//   if (/api/i.test(req.path)) req.session.returnTo = req.path;
+//   next();
+// });
+
+/**
+ * Error Handler.
+ */
+app.use(errorhandler());
+// var env = process.env.NODE_ENV || 'Development';
+// if (env == 'Development'){
+//   app.use(errorhandler({ dumpExceptions : true, showStack : true}));
+// };
+
+// if (env == 'test'){
+//   app.use(errorhandler({ dumpExceptions : true, showStack : true}));
+// };
+
+var server = null
+var httpsServer = null
+
+server = require('http').createServer(app);
+httpsServer = https.createServer(app);
+
+pj = require('./package.json');
+
+require('./core/bootstrap').isEnvSane(server,function(err, port){
+  if(err){
+    console.log(err);
+    process.exit(1);
+  }
+  else
+  {
+    console.log("In Else of Bootstrap.coffee");
+    server.listen((port), function(){
+      console.log("GameFir v"+pj.version+" listening on port %d in %s mode", port, app.settings.env);
+      //console.log("Server's UID is now " + process.getuid());
+    });
+
+    httpsServer.listen((4433), function(){
+      console.log("GameFit Server v"+pj.version+" listening on port %d in %s mode");
+    });
+
+  }
+});
+
+
+module.exports = app;
