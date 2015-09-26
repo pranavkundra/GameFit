fc89ef6c1606d95988f0c997e59999670a9e76a8
Pranav Kundra <pranavkundra@icloud.com>
2015-09-26 11:55:40 -0400

    Added TableView for Abilities

--- ThirdViewController.swift
+++ ThirdViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var abilityTableView: UITableView!
+    var abilityList = ["Super Speed", "Heavy Arm Lifting", "Strength"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        abilityTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "AbilityCell")
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,5 +24,15 @@ class ThirdViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AbilityCell")
+        
+        cell?.textLabel?.text = abilityList[indexPath.row]
+        
+        return cell!
+    }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return abilityList.count
+    }
 }
