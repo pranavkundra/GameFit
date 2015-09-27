3f9e1eb283e12ec1d97b4c98af8fb30b4bca491a
Pranav Kundra <pranavkundra@icloud.com>
2015-09-26 23:01:39 -0400

    More UI Elements added

---
+++ GameFit/AttackListController.swift
@@ -0,0 +1,38 @@
+//
+//  AttackListController.swift
+//  GameFit
+//
+//  Created by Pranav Kundra on 9/26/15.
+//  Copyright © 2015 Pranav Kundra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AttackListController: UITableViewController {
+
+    let attackList = [ ["Attack1", 135], ["Attack2", 335], ["Attack3", 325], ["Attack4", 355],]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return attackList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("AttackListCell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text=attackList[indexPath.row][0] as! String + " - Damage " + String(attackList[indexPath.row][1] as! Int)
+        return cell
+    }
+    
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AttackListCell", forIndexPath: indexPath)
+        
+        cell.accessoryType = .Checkmark
+        cell.textLabel?.text=attackList[indexPath.row][0] as! String + " - Damage " + String(attackList[indexPath.row][1] as! Int)
+    }
+}
