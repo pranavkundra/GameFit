52addc31fec8457bcda96cfa0bb024bc293d0437
Chinmay Rane <chinmayrane17@gmail.com>
2015-09-27 09:53:29 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,48 +1,18 @@
-var bcrypt = require('bcrypt-nodejs');
-var crypto = require('crypto');
 var mongoose = require('mongoose');
 
+
 var userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, lowercase: true },
-  group: String,
-  picture: { type: String, default: '' },
-  mobile_number:{type: Number, unique:true},
+  username: {type: String, unique: true},
+  name: { type: String},
+  score: {type: Number},
+  cyclingDist: {type: Number},
+  climbingDist: {type: Number},
+  heartRate : {type: Number},
+  stepsCount: {type: Number},
+  caloriesBurned: {type:Number},
+  games:[{type: mongoose.Schema.Types.ObjectId, ref:'game'}],
+  friends: [{type: mongoose.Schema.Types.ObjectId, ref:'user'}]
 });
 
-/**
- * Password hash middleware.
- */
-userSchema.pre('save', function(next) {
-  var user = this;
-  if (!user.isModified('password')) return next();
-  bcrypt.genSalt(5, function(err, salt) {
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
-});
 
-/**
- * Helper method for validating user's password.
- */
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
-
-/**
- * Helper method for getting user's gravatar.
- */
-userSchema.methods.gravatar = function(size) {
-  if (!size) size = 200;
-  if (!this.email) return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
-  var md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
-};
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema, 'user');
